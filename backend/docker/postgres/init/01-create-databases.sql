77cf775c03c8cf6148cd16a7c79b12f08e0594b3
Aleksey Zinovjev <alexz04tab2@gmail.com>
2026-03-13 22:49:12 +0700

    Добавлен конфиг для деплоя сервиса настроек

--- backend/docker/postgres/init/01-create-databases.sql
+++ backend/docker/postgres/init/01-create-databases.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE "patternsAuth";
+CREATE DATABASE "patternsUserSetting";
 CREATE DATABASE "patternsUsers";
 CREATE DATABASE "patterns_core_account";
 CREATE DATABASE "patterns_credit";
