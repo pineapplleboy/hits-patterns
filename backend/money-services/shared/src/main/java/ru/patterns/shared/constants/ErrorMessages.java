0ba880f607f9874e922c71967398bb9698ccbb34
Aleksey Zinovjev <alexz04tab2@gmail.com>
2026-03-18 04:15:18 +0700

    Сообщения исключений переведены на русский

--- backend/money-services/shared/src/main/java/ru/patterns/shared/constants/ErrorMessages.java
+++ backend/money-services/shared/src/main/java/ru/patterns/shared/constants/ErrorMessages.java
@@ -4,17 +4,17 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class ErrorMessages {
-    public final String USER_NOT_FOUND = "User not found";
-    public final String FORBIDDEN = "Forbidden";
-    public final String ACCOUNT_NOT_FOUND = "Account not found";
+    public final String USER_NOT_FOUND = "Пользователь не найден";
+    public final String FORBIDDEN = "Доступ запрещён";
+    public final String ACCOUNT_NOT_FOUND = "Счёт не найден";
     public final String UNAUTHORIZED = "Ошибка авторизации!";
-    public final String INCORRECT_REQUEST_AMOUNT = "Incorrect request amount";
-    public final String ACCOUNT_BANNED = "Account banned";
-    public final String ONLY_WITH_RUB = "You can only pay credit with RUB";
-    public final String TRANSFERS_BETWEEN_CURRENCIES_NOT_AVAILABLE = "Transfers between bank accounts to different person with different currency are not available";
-    public final String SERVICE_CURRENTLY_UNAVAILABLE = "Service currently unavailable";
-    public final String CREDIT_RATE_NOT_FOUND = "Credit rate not found";
-    public final String CURRENCY_NOT_FOUND = "Currency not found";
-    public final String INVALID_CREDIT_SUM = "Exceeded credit limit";
-    public final String CURRENCY_NOT_SUPPORTABLE = "Currency is not supportable";
+    public final String INCORRECT_REQUEST_AMOUNT = "Некорректная сумма запроса";
+    public final String ACCOUNT_BANNED = "Счёт заблокирован";
+    public final String ONLY_WITH_RUB = "Данную операцию можно осуществлять только с RUB";
+    public final String TRANSFERS_BETWEEN_CURRENCIES_NOT_AVAILABLE = "Перевод другому человеку в другой валюте запрещён";
+    public final String SERVICE_CURRENTLY_UNAVAILABLE = "Сервис в данный момент недоступен";
+    public final String CREDIT_RATE_NOT_FOUND = "Тарифный план кредита не найден";
+    public final String CURRENCY_NOT_FOUND = "Валюта не найдена";
+    public final String INVALID_CREDIT_SUM = "Некорректный лимит";
+    public final String CURRENCY_NOT_SUPPORTABLE = "Валюта не поддерживается";
 }
