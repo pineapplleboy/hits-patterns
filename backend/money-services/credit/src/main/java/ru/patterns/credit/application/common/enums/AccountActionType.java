ed5b4ea98912927fe965fcb045e87613e7aebfdd
Aleksey Zinovjev <alexz04tab2@gmail.com>
2026-03-21 20:55:45 +0700

    Добавлены типы переводов с одноимённый сервис

--- backend/money-services/credit/src/main/java/ru/patterns/credit/application/common/enums/AccountActionType.java
+++ backend/money-services/credit/src/main/java/ru/patterns/credit/application/common/enums/AccountActionType.java
@@ -1,5 +1,12 @@
 package ru.patterns.credit.application.common.enums;
 
 public enum AccountActionType {
-    OPEN_ACCOUNT
+    OPEN_ACCOUNT,
+    CLOSE_ACCOUNT,
+    TRANSFER,
+    TRANSFER_RECEIVED,
+    TRANSFER_SENT,
+    ACCOUNT_BANNED,
+    ACCOUNT_UNBANNED,
+    CREDIT_DEPT_PERCENT
 }
