ec9a760123d0c7534bc57dbc56f3a9a772f4c9be
Aleksey Zinovjev <alexz04tab2@gmail.com>
2026-03-21 20:42:33 +0700

    Изменён тип поля даты списания в кредитном сервисе

--- backend/money-services/credit/src/main/java/ru/patterns/credit/application/common/model/account/CreditAccountHistoryModel.java
+++ backend/money-services/credit/src/main/java/ru/patterns/credit/application/common/model/account/CreditAccountHistoryModel.java
@@ -1,12 +1,9 @@
 package ru.patterns.credit.application.common.model.account;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import ru.patterns.credit.application.common.model.operation.CreditOperationModel;
-import ru.patterns.credit.application.infrastructure.DurationDeserializer;
 
-import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
@@ -25,8 +22,7 @@ public class CreditAccountHistoryModel {
 
     private int creditRatePercent;
 
-    @JsonDeserialize(using = DurationDeserializer.class)
-    private Duration writeOffPeriod;
+    private String writeOffPeriod;
 
     private Instant nextWriteOffDate;
 
