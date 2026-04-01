96e916851b7f2d78a93eb75240f9a7f4e353408e
Aleksey Zinovjev <alexz04tab2@gmail.com>
2026-04-01 21:16:12 +0700

    Добавлен api-gateway

---
+++ backend/money-services/patterns-gateway/src/main/java/ru/patterns/gateway/PatternsGatewayApplication.java
@@ -0,0 +1,12 @@
+package ru.patterns.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternsGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatternsGatewayApplication.class, args);
+    }
+}
