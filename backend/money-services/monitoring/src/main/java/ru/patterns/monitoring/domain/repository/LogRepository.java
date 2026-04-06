f63899f5edccea8ab2303bd40dc62d70f113c800
Aleksey Zinovjev <alexz04tab2@gmail.com>
2026-04-06 19:11:24 +0700

    Реализовано удаление неактуальных записей в сервисе мониторинга

--- backend/money-services/monitoring/src/main/java/ru/patterns/monitoring/domain/repository/LogRepository.java
+++ backend/money-services/monitoring/src/main/java/ru/patterns/monitoring/domain/repository/LogRepository.java
@@ -3,7 +3,9 @@ package ru.patterns.monitoring.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.patterns.monitoring.domain.entity.Log;
 
+import java.time.Instant;
 import java.util.UUID;
 
 public interface LogRepository extends JpaRepository<Log, UUID> {
+    void deleteByLogTimeBefore(Instant threshold);
 }
--- backend/money-services/monitoring/src/main/java/ru/patterns/monitoring/domain/repository/RequestRepository.java
+++ backend/money-services/monitoring/src/main/java/ru/patterns/monitoring/domain/repository/RequestRepository.java
@@ -3,7 +3,9 @@ package ru.patterns.monitoring.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.patterns.monitoring.domain.entity.Request;
 
+import java.time.Instant;
 import java.util.UUID;
 
 public interface RequestRepository extends JpaRepository<Request, UUID> {
+    void deleteByRequestTimeBefore(Instant threshold);
 }
