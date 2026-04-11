dac84359c440b4149aa9cfb294de90a7d0362a1b
Aleksey Zinovjev <alexz04tab2@gmail.com>
2026-04-11 22:31:27 +0700

    Изменена настройка cors и логов

--- backend/money-services/monitoring/src/main/java/ru/patterns/monitoring/application/kafka/consumer/RequestsListener.java
+++ backend/money-services/monitoring/src/main/java/ru/patterns/monitoring/application/kafka/consumer/RequestsListener.java
@@ -2,48 +2,37 @@ package ru.patterns.monitoring.application.kafka.consumer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 import ru.patterns.monitoring.application.service.MonitoringDataService;
 import ru.patterns.shared.model.monitoring.RequestMonitoringModel;
-import ru.patterns.shared.monitoring.logger.MonitoringLogger;
-import ru.patterns.shared.utility.TraceLogUtility;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class RequestsListener {
 
     private final MonitoringDataService monitoringDataService;
     private final ObjectMapper objectMapper;
-    private final MonitoringLogger monitoringLogger;
 
     @Value("${kafka.consumer.request-topic}")
     private String topic;
 
-    @Value("${service.name}")
-    private String serviceName;
-
     @KafkaListener(topics = "${kafka.consumer.request-topic}", groupId = "${kafka.group}")
-    public void listen(@Payload String message,
-                       @Header(value = "traceId", required = false) String traceId,
-                       Acknowledgment ack) {
+    public void listen(@Payload String message, Acknowledgment ack) {
         try {
-            var logData = TraceLogUtility.createDataForLogs(traceId, "", serviceName, topic, null);
-            monitoringLogger.logInfo(logData, "Получено сообщение из топика " + topic, message, "-");
+            log.info("Получено сообщение из топика {}.", topic);
 
             RequestMonitoringModel msg = objectMapper.readValue(message, RequestMonitoringModel.class);
-
             monitoringDataService.addRequest(msg);
 
             ack.acknowledge();
         } catch (Exception exception) {
-            var logData = TraceLogUtility.createDataForLogs(traceId, "", serviceName, topic, null);
-            monitoringLogger.logError(logData, "Ошибка при обработке сообщения: " + exception.getMessage(), message, "-");
-
+            log.error("Ошибка при обработке сообщения из топика {}: {}", topic, exception.getMessage());
             ack.acknowledge();
         }
     }
